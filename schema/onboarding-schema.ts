d3c80e90bb6230dd65992e38cdb712de9a16660c
James Carl Junio <jamescarljunio@gmail.com>
2025-09-25 01:39:46 +0800

    added verification code schema for onboarding

--- schema/onboarding-schema.ts
+++ schema/onboarding-schema.ts
@@ -7,3 +7,9 @@ export const OnboardingSchema = z.object({
     .min(3, "Alias must be atleast 3 characters long")
     .regex(/^[A-Za-z0-9]+$/, "Alias can only contain letters and numbers"),
 });
+
+export const VerifyCodeSchema = z.object({
+  verificationCode: z
+    .string()
+    .min(6, "Please enter your 6 digit verification code"),
+});
