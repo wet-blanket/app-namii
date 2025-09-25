a4b9966ec0f60106f42317c03f5ef0565d8b9ce8
James Carl Junio <jamescarljunio@gmail.com>
2025-09-25 17:52:12 +0800

    change username format

--- schema/onboarding-schema.ts
+++ schema/onboarding-schema.ts
@@ -4,8 +4,8 @@ export const OnboardingSchema = z.object({
   fullName: z.string().min(1, "Your name is required"),
   userName: z
     .string()
-    .min(3, "Alias must be atleast 3 characters long")
-    .regex(/^[A-Za-z0-9]+$/, "Alias can only contain letters and numbers"),
+    .min(3, "Username must be atleast 3 characters long")
+    .regex(/^[A-Za-z]+$/, "Username can only contain letters"),
 });
 
 export const VerifyCodeSchema = z.object({
