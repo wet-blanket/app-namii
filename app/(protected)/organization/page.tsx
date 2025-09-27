d0999d84c54b120deab50fa8b517db7292ab2d28
James Carl Junio <jamescarljunio@gmail.com>
2025-09-28 02:23:54 +0800

    initial organization route page

---
+++ app/(protected)/organization/page.tsx
@@ -0,0 +1,9 @@
+import { NewOrganizationForm } from "@/app/(protected)/organization/components/new-organization-form";
+
+export default function Organization() {
+  return (
+    <div>
+      <NewOrganizationForm />
+    </div>
+  );
+}
